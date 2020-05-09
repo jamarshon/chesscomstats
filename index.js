b9d8ee17adea2b8026fba2ad6e89a5afbe259418
jamarshon <jasonl96@gmail.com>
2020-05-09 02:57:28 -0700

    large push

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
-const cool = require('cool-ascii-faces')
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const https = require('https')
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .get('/cool', (req, res) => res.send(cool()))
+  .get('/test', (req, res) => {
+    const username = req.query.username;
+    https.get('https://www.chess.com/callback/live/stats/' + username + '/chart?type=blitz', (resp) => {
+      let data = '';
+
+      // A chunk of data has been recieved.
+      resp.on('data', (chunk) => {
+        data += chunk;
+      });
+
+      // The whole response has been received. Print out the result.
+      resp.on('end', () => {
+        res.json(JSON.parse(data));
+      });
+    }).on("error", (err) => {
+      res.status(500).send("Error: " + err.message);
+    });
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
